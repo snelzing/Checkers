3a599970d4b2387318c80485cf3c405dd9e6a94a
snelzing <39928924+snelzing@users.noreply.github.com>
2023-09-18 18:28:47 -0400

    Added the checkersboard component, but it is far from working yet - need more components

---
+++ Checkers/ClientApp/src/components/CheckersBoard.js
@@ -0,0 +1,51 @@
+﻿import React, { Component } from 'react';
+
+class CheckersBoard extends Component {
+    constructor(props) {
+        super(props);
+        // Initialize the game board state here
+        this.state = {
+            // Example: Define the board state, e.g., as a 2D array
+            board: [
+                ['-', 'B', '-', 'B', '-', 'B', '-', 'B'],
+                ['B', '-', 'B', '-', 'B', '-', 'B', '-'],
+                ['-', 'B', '-', 'B', '-', 'B', '-', 'B'],
+                ['-', '-', '-', '-', '-', '-', '-', '-'],
+                ['-', '-', '-', '-', '-', '-', '-', '-'],
+                ['R', '-', 'R', '-', 'R', '-', 'R', '-'],
+                ['-', 'R', '-', 'R', '-', 'R', '-', 'R'],
+                ['R', '-', 'R', '-', 'R', '-', 'R', '-'],
+            ],
+        };
+    }
+
+    render() {
+        return (
+            <div className="checkers-board">
+                {this.state.board.map((row, rowIndex) => (
+                    <div key={rowIndex} className="row">
+                        {row.map((cell, columnIndex) => (
+                            <div
+                                key={columnIndex}
+                                className={`cell ${cell === '-' ? 'empty' : cell === 'B' ? 'black-piece' : 'red-piece'}`}
+                                // Add click event handler for making moves
+                                onClick={() => this.handleCellClick(rowIndex, columnIndex)}
+                            >
+                                {cell !== '-' && <div className="piece">{cell}</div>}
+                            </div>
+                        ))}
+                    </div>
+                ))}
+            </div>
+        );
+    }
+
+    // Define the click event handler for making moves
+    handleCellClick(row, col) {
+        // Implement move logic here
+        // Update the board state based on the player's move
+        // You can also check for valid moves and implement game rules
+    }
+}
+
+export default CheckersBoard;
